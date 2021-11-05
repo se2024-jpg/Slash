02b67286d51f7646442f74bf77d26c45e90e23a6
antgad <anantgadodia.17@gmail.com>
2021-11-05 09:11:33 -0700

    Update test_formatResults.py

--- tests/test_formatResults.py
+++ tests/test_formatResults.py
@@ -20,16 +20,16 @@ def test_sortList():
     assert formatter.sortList(arr, "pr", False) == ansArr
     assert formatter.sortList(arr, "pr", True) == revAnsArr
 
- def test_formatResults():
-     """
-     Checks the formatResults function
-     """
-     titles = [BeautifulSoup('<div class="someclass">title  </div>', "html.parser")]
-     prices = [BeautifulSoup('<div class="someclass">$0.99  </div>', "html.parser")]
-     links = []
+def test_formatResults():
+    """
+    Checks the formatResults function
+    """
+    titles = [BeautifulSoup('<div class="someclass">title  </div>', "html.parser")]
+    prices = [BeautifulSoup('<div class="someclass">$0.99  </div>', "html.parser")]
+    links = []
 
-     product = formatter.formatResult("example", titles, prices, links)
-     ans = {"title":"title", "price":"$0.99", "website":"example"}
-     print(product["website"], ans["website"])
+    product = formatter.formatResult("example", titles, prices, links)
+    ans = {"title":"title", "price":"$0.99", "website":"example"}
+    print(product["website"], ans["website"])
 
-     assert product["title"] == ans["title"] and product["price"] == ans["price"] and product["website"] == ans["website"]
+    assert product["title"] == ans["title"] and product["price"] == ans["price"] and product["website"] == ans["website"]
