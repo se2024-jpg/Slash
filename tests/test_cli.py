60338b2fb992871c6c7f43162177ae1fbe6f2716
Parth Parikh <parthparikh1999p@gmail.com>
2021-12-02 02:02:14 -0500

    Added tests for extract lists and search_fn

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,7 @@ import random
 import string
 import os
 import json
+import pytest
 
 def test_set_player_name(monkeypatch):
     fv = full_version.full_version()
@@ -22,5 +23,37 @@ def test_set_player_name(monkeypatch):
             email = data["email"]
         assert fv.login() == (name, email)
 
-def test_search_fn(monkeypatch):
-    answers = iter([1, 3])
+def test_extract_list(monkeypatch, capfd):
+    fv = full_version.full_version()
+    wishlist_index = 0
+    # Create a new wishlist
+    answers = iter([2, wishlist_index, 3])
+    monkeypatch.setattr('builtins.input', lambda name: next(answers))
+    fv.extract_list()
+    out, err = capfd.readouterr()
+    assert "Wishlists" in out
+
+    # Adds an item to wish list
+    answers = iter(["socks", 1, wishlist_index, 5])
+    monkeypatch.setattr('builtins.input', lambda name: next(answers))
+    fv.search_fn()
+    out, err = capfd.readouterr()
+    assert "Item added successfully" in out
+
+    answers = iter([1, wishlist_index, 3])
+    monkeypatch.setattr('builtins.input', lambda name: next(answers))
+    fv.extract_list()
+    out, err = capfd.readouterr()
+    assert "Wishlists" in out
+
+    # First "3" indicates deleting the index
+    # Second "3" indicates closing the menu
+    # Deletes the 0th index
+    answers = iter([3, wishlist_index, 3])
+    monkeypatch.setattr('builtins.input', lambda name: next(answers))
+    fv.extract_list()
+
+    with pytest.raises(IndexError):
+        answers = iter([1, wishlist_index, 3])
+        monkeypatch.setattr('builtins.input', lambda name: next(answers))
+        fv.extract_list()
