5c48dfd8867fa7af44e75b7ef6183dfeccfeaa70
sairajzero <sairaj0630@gmail.com>
2023-10-18 19:46:26 -0400

    Updating login test case

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,19 +10,19 @@ def test_set_player_name(monkeypatch):
     fv = full_version.full_version()
     if not os.path.exists(fv.user_data):
         name = "".join(random.choices(string.ascii_lowercase, k=5))
-        email = "".join(random.choices(string.ascii_lowercase, k=3)) + "@mail.com"
+        #email = "".join(random.choices(string.ascii_lowercase, k=3)) + "@mail.com"
         
-        answers = iter([name, email])
+        answers = iter([name])
 
         monkeypatch.setattr('builtins.input', lambda name: next(answers))
 
-        assert fv.login() == (name, email)
+        assert fv.login() == name
     else:
         with open(fv.user_data) as json_file:
             data = json.load(json_file)
             name = data["name"]
-            email = data["email"]
-        assert fv.login() == (name, email)
+            # email = data["email"]
+        assert fv.login() == name
 
 def test_extract_list(monkeypatch, capfd):
     fv = full_version.full_version()
