337786e63e47025f0b844fbade4460f351ef8798
aadithya-naresh <aadithya.naresh5@gmail.com>
2023-11-22 15:31:27 -0500

    Added Test cases for scraping function

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,4 +1,8 @@
 from src.modules import scraper
+import sys
+import os
+current_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.abspath(os.path.join(current_dir, ".."))
 
 def test_filter():
     data = [ {"price": "$10", "rating": "4.5"}, {"price": "$20", "rating": "4.0"}, {"price": "$30", "rating": "3.5"}, {"price": "$40", "rating": "5"} ]
@@ -17,3 +21,39 @@ def test_driver():
     scraper.driver("socks", None, ui=True, sort="raasc")
     scraper.driver("socks", None, ui=True, sort="pasc")
     scraper.driver("socks", None, ui=True, sort="asc")
+
+def test_amazon_scraper():
+    products = scraper.searchAmazon('table',False,None)
+    price = scraper.amazon_scraper(products[0]['link'])
+
+    assert price is None or type(price) == str
+
+def test_walmart_scraper():
+    products = scraper.searchWalmart('table',False,None)
+    price = scraper.walmart_scraper(products[0]['link'])
+
+    assert price is None or type(price) == str
+
+def test_ebay_scraper():
+    products = scraper.searchEbay('table',False,None)
+    price = scraper.ebay_scraper(products[0]['link'])
+
+    assert price is None or type(price) == str
+
+def test_bestbuy_scraper():
+    products = scraper.searchBestbuy('table',False,None)
+    price = scraper.bestbuy_scraper(products[0]['link'])
+
+    assert price is None or type(price) == str
+
+def test_target_scraper():
+    products = scraper.searchTarget('table',False,None)
+    price = scraper.target_scraper(products[0]['link'])
+
+    assert price is None or type(price) == str
+
+def test_google_scraper():
+    products = scraper.searchGoogleShopping('table',False,None)
+    price = scraper.google_scraper(products[0]['link'])
+
+    assert price is None or type(price) == str
