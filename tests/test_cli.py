667d1218bb60d9ac286a3f5be0b1dbc4561b8496
sathiya06 <sathiyavenkat06@gmail.com>
2023-11-27 19:32:43 -0500

    Removed extract list test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -44,44 +44,6 @@ def test_change_user(monkeypatch, capfd):
     out, err = capfd.readouterr()
     assert "Welcome" in out
 
-def test_extract_list(monkeypatch, capfd):
-    fv = full_version.full_version()
-    features.create_user('test','pass')
-    fv.name = 'test'
-    fv.password = 'pass'
-    wishlist_index = 0
-    # Create a new wishlist
-    answers = iter([2, wishlist_index, 3])
-    monkeypatch.setattr('builtins.input', lambda name: next(answers))
-    fv.extract_list()
-    out, err = capfd.readouterr()
-    assert "Wishlists" in out
-
-    # Adds an item to wish list
-    answers = iter(["socks", 1, wishlist_index, 5])
-    monkeypatch.setattr('builtins.input', lambda name: next(answers))
-    fv.search_fn()
-    out, err = capfd.readouterr()
-    assert "Item added successfully" in out
-
-    answers = iter([1, wishlist_index, 3])
-    monkeypatch.setattr('builtins.input', lambda name: next(answers))
-    fv.extract_list()
-    out, err = capfd.readouterr()
-    assert "Wishlists" in out
-
-    # First "3" indicates deleting the index
-    # Second "3" indicates closing the menu
-    # Deletes the 0th index
-    answers = iter([3, wishlist_index, 3])
-    monkeypatch.setattr('builtins.input', lambda name: next(answers))
-    fv.extract_list()
-
-    with pytest.raises(IndexError):
-        answers = iter([1, 33, 3])
-        monkeypatch.setattr('builtins.input', lambda name: next(answers))
-        fv.extract_list()
-
 def test_csv_writer():
     x = csv_writer.write_csv([{"name": "parth", "surname": "parikh", "age": 10}], "Names", ".")
     assert x[:5] == "Names"
